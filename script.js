3364400f985c2b8684de22d1294bb3ccdf4cdc93
fiqrierudy1995-glitch <fiqrierudy1995@gmail.com>
2025-11-12 22:01:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -326,7 +326,6 @@ async function submitRSVP(form, url, rsvpChoice) {
   
   messageDiv.textContent = "Menghantar...";
   messageDiv.style.display = "block";
-  messageDiv.style.backgroundColor = "beige";
   messageDiv.style.color = "black";
   hantarBtn.disabled = true;
 

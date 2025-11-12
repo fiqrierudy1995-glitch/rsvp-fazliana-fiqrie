25e20c71b9a34b2867f0a85afb3dcd1404bf6be9
fiqrierudy1995-glitch <fiqrierudy1995@gmail.com>
2025-11-12 22:08:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -344,25 +344,13 @@ async function submitRSVP(form, url, rsvpChoice) {
     });
 
     const data = await response.json();
-if (data.status === "success") {
-  messageDiv.textContent = rsvpChoice === "Hadir"
-    ? "Terima kasih! Kehadiran anda telah disahkan."
-    : "Terima kasih! Kami memahami anda tidak dapat hadir pada majlis.";
-
-  // Make background transparent
-  messageDiv.style.backgroundColor = "transparent";
-
-  // Keep text color if you want
-  messageDiv.style.color = rsvpChoice === "Hadir" ? "#000" : "#363636";
-
-  form.reset();
-
-  // Auto-close the popup after 2 seconds
-  setTimeout(() => {
-    popupRSVP.style.display = "none";
-    resetRSVP();
-  }, 2000);
-}
+    if (data.status === "success") {
+      messageDiv.textContent = rsvpChoice === "Hadir"
+        ? "Terima kasih! Kehadiran anda telah disahkan."
+        : "Terima kasih! Kami memahami anda tidak dapat hadir pada majlis.";
+      messageDiv.style.backgroundColor = rsvpChoice === "Hadir" ? "#e7f1f8" : "#ffdd57";
+      messageDiv.style.color = rsvpChoice === "Hadir" ? "#000" : "#363636";
+      form.reset(); transparent bg
 
 
        // ✅ Auto-close the popup after 2 seconds
